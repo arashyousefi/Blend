232f52f718b6e203a1bba3511058859b7ba52608
arash <arashyou3fi@gmail.com>
2017-01-30 15:15:42 +0330

    added flex scanner and token classes

---
+++ src/Token.java
@@ -0,0 +1,11 @@
+/**
+ * Created by arash on 1/30/17.
+ */
+public class Token {
+    String type, id;
+
+    public Token(String type, String id) {
+        this.type = type;
+        this.id = id;
+    }
+}
