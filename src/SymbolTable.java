766518b95c7f9f594bbd60738fc4b1f80392f990
arash <arashyou3fi@gmail.com>
2017-01-31 17:46:44 +0330

    symbol table added

---
+++ src/SymbolTable.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class SymbolTable {
+    ArrayList<SymbolTableEntry> symbols = new ArrayList<>();
+    SymbolTable parent; // scope in which this symbol table is defined in
+
+    public SymbolTable(SymbolTable parent) {
+        this.parent = parent;
+    }
+
+    public SymbolTable() {
+    }
+
+    public SymbolTableEntry findSymbol(String name, boolean findInParent) {
+        for (SymbolTableEntry symbolTableEntry : symbols)
+            if (symbolTableEntry.name.equals(name))
+                return symbolTableEntry;
+        if (parent != null && findInParent)
+            return parent.findSymbol(name, true);
+        return null;
+    }
+
+    public SymbolTableEntry addSymbol(String name, int type) {
+        SymbolTableEntry symbolTableEntry = new SymbolTableEntry(name, type);
+        symbols.add(symbolTableEntry);
+        return symbolTableEntry;
+    }
+}
